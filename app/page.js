cb9f6b6fc9a915bd737be10c813cff0b55636e46
Arnab Pramanik <125970454+AP102468@users.noreply.github.com>
2024-01-27 22:06:14 +0530

    Add files via upload

---
+++ app/page.js
@@ -0,0 +1,18 @@
+"use client"
+import Header from '@/Components/Header'
+import Home from '@/Components/Home'
+import React from 'react'
+
+
+const page = () => {
+  return (
+    <>
+      <Header/>
+      <Home/>
+    
+      WELCOME TO THE TO-DO APP
+    </>
+  )
+}
+
+export default page;
